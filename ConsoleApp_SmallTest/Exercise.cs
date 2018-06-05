773ef14fc4bdcec8ba89d1c4d6ab7a5ddc7de825
HsinHanShen <hhs710710@gmail.com>
2018-06-05 16:32:12 +0800

    finish console interface and exercise-01

---
+++ ConsoleApp_SmallTest/Exercise.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_SmallTest
+{
+    class Exercise
+    {
+        public int Ex01_PrintStar()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 5; j > i; j--)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            
+
+            return 0;
+        }
+
+
+
+    }
+}
--- ConsoleApp_SmallTest/Program.cs
+++ ConsoleApp_SmallTest/Program.cs
@@ -6,27 +6,69 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp_SmallTest
 {
+    
+
     class Program
     {
         static void Main(string[] args)
         {
-            char key = (char)0;
-            
+            string inStr = "";
+            bool isExit;
+            Exercise myEx = new Exercise();
 
-            int input = 0;
-            Console.WriteLine("=== Small Test App ===");
-            Console.WriteLine("\t 1) print test.");
-            Console.WriteLine("\t 2) xxx test.");
-            Console.Write("\t Please select number : ");
-            key = Convert.ToChar( Console.Read() );
+            do
+            {
+                Console.WriteLine("=== Small Test App ===");
+                Console.WriteLine("\t 1) print star.");
+                Console.WriteLine("\t 2) xxx test.");
+                Console.Write("\t Please select number : ");
 
-            string str = input.ToString();
-            char c = Convert.ToChar(input);
-            Console.WriteLine("\t user input = {0}", c);
+                inStr = Console.ReadLine();
+                switch (inStr)
+                {
+                    case "2":
+                        Console.WriteLine("<<< Exercise_{0} start >>>", inStr);
 
-            Console.WriteLine("\n\n");
-            Console.WriteLine("Press any KEY exit.");
-            Console.ReadKey();
+                        Console.WriteLine("====== Exercise end ======");
+                        break;
+                    case "3":
+                        Console.WriteLine("<<< Exercise_{0} start >>>", inStr);
+
+                        Console.WriteLine("====== Exercise end ======");
+                        break;
+                    case "4":
+                        Console.WriteLine("<<< Exercise_{0} start >>>", inStr);
+
+                        Console.WriteLine("====== Exercise end ======");
+                        break;
+                    case "1":
+                        Console.WriteLine("<<< Exercise_{0} start >>>", inStr);
+                        myEx.Ex01_PrintStar();
+                        Console.WriteLine("====== Exercise end ======");
+                        break;
+                }
+
+
+                Console.Write("Press 'E' to exit: ");
+
+
+                inStr = Console.ReadLine();
+                isExit = inStr.Equals("E");
+                if (isExit == true)
+                    Console.WriteLine("\n ...Goodbye");
+                else
+                    Console.WriteLine("\nRepert App ...");
+
+            } while (isExit == false);           
+
+
+
+
+
+
+
+
+
         }
     }
 }
